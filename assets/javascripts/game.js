c06f98ad46c1af97ca2ee6ee6a7c94871abb643c
Nazam <nazamzahoor@yahoo.com>
2018-06-12 23:36:38 -0700

    design is done

---
+++ assets/javascripts/game.js
@@ -0,0 +1,13 @@
+var gameObject = {
+    currentletter: "",
+
+    allGuesses : [],
+    incorrectGuesses : [],
+    correctGuesses : [],
+    correctGuessesInOrder : [],
+
+    bandsArray: ["QUEEN","ACDC","METALLICA","JOURNEY","REM","POISON","BLONDIE","GENESIS","DEVO"],
+    randomWord: "",
+    bandsLetter:[],
+
+}
